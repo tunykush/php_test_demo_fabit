c2e1240d5e568cdff7e8bc6dadf107d0f7282980
tunykush <kyoukkuitsuki@gmail.com>
2024-10-18 10:00:59 +1100

    update news

--- assets/js/homejs/update-data.js
+++ assets/js/homejs/update-data.js
@@ -1,20 +1,20 @@
 const UPDATE_DATA = [
   {
     imgSrc: "./images/profile-2.jpg",
-    profileName: "Mike Tyson",
-    message: "Co-Founder & Chief Customer Service Officer",
-    updatedTime: " ",
+    profileName: "BTC big pump",
+    message: "Analysts tell Decrypt that a number of factors....",
+    updatedTime: "2 hours ago",
   },
   {
     imgSrc: "./images/profile-3.jpg",
-    profileName: "Diana Ayi",
-    message: "Alpha Droper|| #KOL|| Trader since 2021|| holder",
-    updatedTime: " ",
+    profileName: "Sam Altman's WLD token falls 10%..",
+    message: "Sam Altman and Alex Blania announced the company’s future vision and....",
+    updatedTime: "1 hour ago",
   },
   {
     imgSrc: "./images/profile-4.jpg",
-    profileName: "Mandy Roy",
-    message: "I am trading solely on",
-    updatedTime: " ",
+    profileName: "Whale Alert",
+    message: "12,726 #ETH (33,193,316 USD) transferred from unknown wallet to #Coinbase",
+    updatedTime: "Now",
   },
 ];
